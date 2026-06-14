914d36cbb339f11eefec8bd88fdc1f9a8c9cd2c7
monsectzz <monserratcortezz05@gmail.com>
2026-06-14 14:30:21 -0600

    Estructura base: definicion de constantes y tipos en juego.h

--- juego.h
+++ juego.h
@@ -1,11 +1,10 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 
-#define FILAS 60 
-#define COLS 60 
+#define FILAS 61
+#define COLS 60
 #define VISTA 20
 
-//Caracteres para representar el mapa
 #define PARED   '#'
 #define CAMINO  '.'
 #define JUGADOR 'P'
@@ -14,7 +13,6 @@
 #define PUERTA  'D'
 #define SALIDA  'E'
 
-//Estado del Jugador
 typedef struct {
     int fila;
     int col;
@@ -23,17 +21,18 @@ typedef struct {
     int pasos;
 } Jugador;
 
-//Funciones en C
+/* Funciones en C */
 void imprimir_mapa(char mapa[FILAS][COLS], Jugador *j, int nivel);
-void mover_jugador(char mapa[FILAS][COLS], Jugador *j);
+int  mover_jugador(char mapa[FILAS][COLS], Jugador *j);
 void resumen_nivel(Jugador *j, int total_monedas, int nivel);
-void resumen_final(int monedas_total, int pasos_total, int puntaje);
+void resumen_final(int monedas_total, int monedas_posibles,
+                   int pasos_total, int puntaje);
 
-//Funciones en NASM
+/* Funciones en NASM */
 extern long long contar_caracter(char *mapa, int total, char c);
 extern long long validar_movimiento(char *mapa, int cols, int fila, int col);
 extern long long calcular_puntaje(int monedas, int pasos, int niveles);
 extern long long detectar_objeto(char *mapa, int cols, int fila, int col, char obj);
 extern long long contar_libres(char *mapa, int total);
 
-#endif 
+#endif
